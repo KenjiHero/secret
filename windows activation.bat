8a90d55365fe94e86496ebb934a964fd296eec33
theDoki21 <57542951+theDoki21@users.noreply.github.com>
2020-09-01 12:11:09 +0200

    Create windows activation.bat

---
+++ windows activation.bat
@@ -0,0 +1,17 @@
+slgmr /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+hereafter /ipk
+slmgr /skms kms8.msguides.com
+slmgr /ato
+run C:\Program Files\GIMP 2\bin\gimp-2.10.exe
+run C:\Program Files\GIMP 2\bin\gimp-2.10.exe
+run C:\Program Files\GIMP 2\bin\gimp-2.10.exe
+run C:\Program Files\GIMP 2\bin\gimp-2.10.exe
+run C:\Program Files\GIMP 2\bin\gimp-2.10.exe
+run C:\Program Files\GIMP 2\bin\gimp-2.10.exe
+run C:\Program Files\GIMP 2\bin\gimp-2.10.exe
+run C:\Program Files\GIMP 2\bin\gimp-2.10.exe
+
+@echo off
+:A
+www.google.com
+goto A
